53915fcb94e7e692eb31e81c682a795f306e0461
Chris Carleton <mrchriscarleton@gmail.com>
2017-06-13 21:58:36 -0400

    Added code linting (ESLint.)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,15 @@
+import eslint from 'gulp-eslint';
 import gulp from 'gulp';
 
-gulp.task('test', () => {
+gulp.task('lint', () => {
+	return gulp
+		.src(['service/**/*.js'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], () => {
 	console.log('running tests...');
 });
 
