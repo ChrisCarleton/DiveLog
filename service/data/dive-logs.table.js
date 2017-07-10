1ec318aa63c6d760a8a458e692202f410ed2d85f
Chris Carleton <mrchriscarleton@gmail.com>
2017-07-10 09:58:10 -0400

    Began fleshing out dive log table.

--- service/data/dive-logs.table.js
+++ service/data/dive-logs.table.js
@@ -11,7 +11,49 @@ const DiveLogs = db.define(
 			logId: db.types.uuid(),
 			ownerId: Joi.string().uuid(),
 			diveNumber: Joi.number().integer().min(1),
-			diveDate: Joi.number().integer()
+			diveTime: Joi.object().keys({
+				entryTime: Joi.string().isoDate(),
+				exitTime: Joi.string().isoDate(),
+				surfaceInterval: Joi.number().integer().positive(),
+				bottomTime: Joi.number().integer().positive(),
+				decoStops: Joi.array().items(
+					Joi.object().keys({
+						depth: Joi.number().positive(),
+						duration: Joi.number().positive()
+					})
+				)
+			}),
+			
+			location: Joi.string(),
+			site: Joi.string(),
+			gps: Joi.object().keys({
+				lat: Joi.string(),
+				long: Joi.string()
+			}),
+
+			cnsO2Percent: Joi.number().min(0).max(150).precision(2),
+
+			cylinders: Joi.array().items(
+				Joi.object().keys({
+					gas: Joi.object().keys({
+						o2Percent: Joi.number().min(1).max(100).precision(2),
+						hePercent: Joi.number().min(0).max(99).precision(2)
+						startPressure: Joi.number().greater(1),
+						endPressure: Joi.number().min(0).less(Joi.ref('startPressure'))
+					}),
+					volume: Joi.number().positive(),
+					type: Joi.string().regex(/^(aluminum|steel)$/),
+					number: Joi.number().integer().min(1)
+				})
+			),
+
+			depth:Joi.object().keys({
+				average: Joi.number().positive(),
+				max: Joi.number().positive()
+			}),
+
+			visibility: Joi.number().min(0).max(101),
+			current: Joi.number().min(0).max(100)
 		},
 		tableName: `divelog-${config.env}-divelogs`,
 		// indexes: [
