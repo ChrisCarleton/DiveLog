5bbfb9c76e33e921747272d109c96cd86c272504
Chris Carleton <mrchriscarleton@gmail.com>
2017-07-19 10:53:46 -0400

    Fixing how Docker images get pushed.

--- deploy/push-docker-image.sh
+++ deploy/push-docker-image.sh
@@ -2,5 +2,5 @@
 set -e
 
 $(aws ecr get-login --region us-east-1)
-docker tag divelog:$1 961445962603.dkr.ecr.us-east-1.amazonaws.com/divelog:$1
+docker tag divelog:latest 961445962603.dkr.ecr.us-east-1.amazonaws.com/divelog:$1
 docker push 961445962603.dkr.ecr.us-east-1.amazonaws.com/divelog:$1
