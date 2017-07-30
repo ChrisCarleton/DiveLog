0d0040e8f472b936fd7dcb68b1f7efa1e9a089d5
Chris Carleton <mrchriscarleton@gmail.com>
2017-07-30 00:37:40 -0400

    Moved phantom js to an S3 bucket because I am tired of build failures.

--- deploy/install-phantomjs.sh
+++ deploy/install-phantomjs.sh
@@ -2,7 +2,7 @@
 set -e
 
 mkdir phantom-temp/
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O phantom-temp/phantomjs.tar.bz2
+aws s3 cp s3://divelog-cloudformation-templates/phantomjs-2.1.1-linux-x86_64.tar.bz2 ./phantom-temp/phantomjs-2.1.1-linux-x86_64.tar.bz2
 tar -xf phantom-temp/phantomjs.tar.bz2 -C /usr/bin
 ln -s /usr/bin/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
 phantomjs -v
