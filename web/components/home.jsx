3c9db00dc418a10947468fb3829dd125992f0ea0
Chris Carleton <mrchriscarleton@gmail.com>
2017-08-31 08:47:08 -0400

    New slick background and started fleshing out home page.

--- web/components/home.jsx
+++ web/components/home.jsx
@@ -1,14 +1,22 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 import {
-	PageHeader
+	Button,
+	Jumbotron
 } from 'react-bootstrap';
 
 class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<PageHeader>Bottom Time</PageHeader>
+				<Jumbotron>
+					<h1>Bottom Time<br/>
+						<small>An online log book for scuba divers</small>
+					</h1>
+					<Button bsStyle="primary" bsSize="large">Log In Now</Button>&nbsp;
+					Don't have an account? <Link to="/signup">Sign up!</Link>
+				</Jumbotron>
 
 				<p>
 					An online logbook for scuba divers.
